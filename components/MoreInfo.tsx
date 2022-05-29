a5703c59887a83cfb7dbd4bbdf4c7b37d25db22f
Keeney <nkeeney@oceanics.io>
2022-05-29 14:48:53 -0400

    data local to function, gql on MoreInfo

--- components/MoreInfo.tsx
+++ components/MoreInfo.tsx
@@ -1,7 +1,8 @@
 import * as React from "react";
-
+import { useEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { useColorScheme } from "../hooks/useColorScheme";
+import { useQuery, gql } from "@apollo/client";
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import { Pressable } from "react-native";
@@ -9,12 +10,19 @@ import { Pressable } from "react-native";
 import Colors from "../constants/Colors";
 import type { RootStackParamList } from "../types";
 
+const HELLO_QUERY = gql`{ hello }`;
+
 const MoreInfo =
   (destination: keyof RootStackParamList) =>
   () => {
     const { navigate } = useNavigation();
     const colorScheme = useColorScheme();
+    const { error, data } = useQuery(HELLO_QUERY);
 
+    useEffect(() => {
+      console.log({error, data})
+    }, [error, data])
+    
     return (
       <Pressable
         onPress={() => navigate(destination)}
